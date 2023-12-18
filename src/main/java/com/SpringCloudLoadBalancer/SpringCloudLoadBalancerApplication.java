19a634063809da1439c764a6167f408048eec028
pratyush021 <probohidar2016@gmail.com>
2023-12-18 20:28:57 +0530

    project setup

---
+++ src/main/java/com/SpringCloudLoadBalancer/SpringCloudLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.SpringCloudLoadBalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLoadBalancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringCloudLoadBalancer/SpringCloudLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringCloudLoadBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
