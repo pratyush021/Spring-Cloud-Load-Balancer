c5ac7ae4f2a83ff5a0ceb76b7402e7eec3e7d6b1
pratyush021 <probohidar2016@gmail.com>
2023-12-18 21:12:16 +0530

    added endpoints

--- src/main/java/com/SpringCloudLoadBalancer/SpringCloudLoadBalancerApplication.java
+++ src/main/java/com/SpringCloudLoadBalancer/SpringCloudLoadBalancerApplication.java
@@ -1,13 +1,41 @@
 package com.SpringCloudLoadBalancer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
 @SpringBootApplication
 public class SpringCloudLoadBalancerApplication {
-
+	private static Logger log = LoggerFactory.getLogger(SpringCloudLoadBalancerApplication.class);
 	public static void main(String[] args) {
+		System.out.println("test 1");
 		SpringApplication.run(SpringCloudLoadBalancerApplication.class, args);
 	}
 
+	@GetMapping("/greetings")
+	public String greet() {
+		log.info("Access /greetings");
+
+		List<String> greetings = Arrays.asList("Greetings", "Aloha", "Salutations");
+
+		Random rand = new Random();
+		int randomNum = rand.nextInt(greetings.size());
+		return greetings.get(randomNum);
+
+	}
+	@GetMapping("/")
+	public String home() {
+		log.info("Accecss /");
+		return "Hi";
+	}
 }
+
+
---
+++ src/main/java/com/SpringCloudLoadBalancer/UserApplication.java
@@ -0,0 +1,17 @@
+package com.SpringCloudLoadBalancer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+@RestController
+public class UserApplication {
+//    private final WebClient.Builder loadBalancedClientBuilder;
+//
+//    private final ReactorLoadBalancerExchangeFilterFunction lbFunction;
+//
+//    public UserApplication(WebClient.Builder webClientBuilder, ReactorLoadBalancerExchangeFilterFunction lbFunction ) {
+//
+//    }
+}
